c5eefe5d1c762f2126709b0c46db53a31ec76590
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-01-11 17:22:50 +0100

    add add form formController

--- Form/formController.js
+++ Form/formController.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const FormService = require("./FormService");
+const FormService = require("./formService");
+
+router.post("/addForm", FormService.addForm);
+
+module.exports = router;
